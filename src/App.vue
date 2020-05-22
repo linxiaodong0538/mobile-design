a80c8abe505733f375bf8d4b309ea708b4a206be
linxin <734127061@qq.com>
2020-05-22 11:50:46 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function() {
+    console.log("App Launch");
+    const token = uni.getStorageSync("token");
+    if (!token) {
+      uni.reLaunch({
+        url: "/pages/login/index"
+      });
+    }
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import store from './store'
+import globalPlugin from './utils/plugins/global'
 
 Vue.config.productionTip = false
-
 App.mpType = 'app'
 
+Vue.use(globalPlugin)
+
 const app = new Vue({
-  ...App
+    ...App
 })
 app.$mount()
